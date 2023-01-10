2f415a4fc5923f3645ec8062e834c05051d707f2
yuinra <nam.tp194631@sis.hust.edu.vn>
2023-01-10 10:04:50 +0000

    add home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+   root to: 'static_pages#home'
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   devise_for :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
