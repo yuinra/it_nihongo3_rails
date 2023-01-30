cc4cc90f7b586b3108a6a4dc8ac9d5c13cc592b1
yuinra <nam.tp194631@sis.hust.edu.vn>
2023-01-30 06:45:31 +0000

    Add avatar on users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    validates :name, presence: true
+  validates :name, presence: true
   validates :age, presence: true
+  
+    has_one_attached :avatar
+    after_commit :add_default_avatar, on: %i[create update]
+
+    def avatar_thumbnail
+        if avatar.attached?
+            avatar.variant(resize: "150x150!").processed 
+        else 
+
+        end
+    end
+
+    private
+    def add_default_avatar
+        unless avatar.attached?
+        avatar.attach(
+            io: File.open(Rails.root.join(
+                    'app', 'assets', 'images', ''
+                )
+            ), filename: '',
+            content_type: 'image/jpg'
+        )
+        end
+    end
 end
